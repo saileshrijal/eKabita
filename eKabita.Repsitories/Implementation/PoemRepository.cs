a0b6a3f4a0972274a1c1550211419b0eff51aebe
Sailesh Rijal <saileshrijal1@gmail.com>
2022-10-01 08:37:03 +0545

    home Service added

--- eKabita.Repsitories/Implementation/PoemRepository.cs
+++ eKabita.Repsitories/Implementation/PoemRepository.cs
@@ -1,18 +1,23 @@
 ﻿using eKabita.Data;
 using eKabita.Models;
 using eKabita.Repsitories.Interface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace eKabita.Repsitories.Implementation
 {
     internal class PoemRepository : GenericRepository<Poem>, IPoemRepository
     {
+        private readonly ApplicationDbContext _context;
+
         public PoemRepository(ApplicationDbContext context) : base(context)
         {
+            _context = context;
+        }
+
+        public override async Task<List<Poem>> GetAll()
+        {
+            var poems = await _context.Poems.Include(x=>x.ApplicationUser).OrderByDescending(x=>x.CreatedDate).ToListAsync();
+            return poems;
         }
     }
 }
